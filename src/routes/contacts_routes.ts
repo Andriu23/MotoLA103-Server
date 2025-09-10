30d0eb37bc54e2c6a2b7301716f7584d5e232721
Andres Satoba <satokandre@outlook.com>
2025-09-09 21:01:34 -0500

    Actualizacón de Autenticación de los Contactos

--- src/routes/contacts_routes.ts
+++ src/routes/contacts_routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { crearContact, deleteContact, getContactById, getContacts, updateContact } from "../controllers/contact_controller";
+import { authenticateToken } from "../middleware/user_middleware";
 
 const contactsRoutes = Router();
 
 contactsRoutes.post('/contacts', crearContact);
 contactsRoutes.get('/contacts', getContacts);
-contactsRoutes.put('/contacts', updateContact);
-contactsRoutes.delete('/contacts/:id', deleteContact);
+contactsRoutes.put('/contacts', authenticateToken, updateContact);
+contactsRoutes.delete('/contacts/:id', authenticateToken, deleteContact);
 contactsRoutes.get('/contacts/:id', getContactById);
 
 export {contactsRoutes};
