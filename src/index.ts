10a9f771be6714f89f5cf98343b733d352408311
Andres Satoba <satokandre@outlook.com>
2025-09-12 13:32:59 -0500

    Modificando index.ts para producción

--- src/index.ts
+++ src/index.ts
@@ -26,5 +26,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Estoy corriendo en el puerto ${port}`);
+    return console.log(`Estoy corriendo en el puerto ${port}`);
 });
